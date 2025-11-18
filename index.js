5a166b316499875f23d2cc388cb0386f642dc588
nimsk0 <nikolamisk@gmail.com>
2025-11-18 17:07:30 +0100

    Added frontend

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('data', (request, response) => { return JSON.stringify(request.body) })
 
